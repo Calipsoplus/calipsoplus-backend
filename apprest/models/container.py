834e10355e3744f4b21760783bdac5d581bfec9b
acampsm <acampsm@cells.es>
2018-05-30 11:11:37 +0200

    Adding field for maxium machine number per user

--- apprest/models/container.py
+++ apprest/models/container.py
@@ -1,6 +1,8 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
 
+from calipsoplus.settings import MAX_CONTAINER_PER_USER
+
 
 class CalipsoContainer(models.Model):
     calipso_user = models.CharField(max_length=255)
@@ -13,12 +15,12 @@ class CalipsoContainer(models.Model):
     guacamole_username = models.CharField(max_length=255, blank=True)
     guacamole_password = models.CharField(max_length=255, blank=True)
     vnc_password = models.CharField(max_length=255, blank=True)
+    max_num_container = models.IntegerField(default=MAX_CONTAINER_PER_USER)
 
     history = HistoricalRecords()
 
     class Meta:
         db_table = 'calipso_containers'
-        # unique_together = (('calipso_user', 'calipso_experiment'),)
 
     def __str__(self):
         return self.container_name
