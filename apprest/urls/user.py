46339edcbd195fecd1e1fd00325ba00e56ff1e9c
Alex Camps <acampsm@cells.es>
2018-06-05 15:16:31 +0000

    urls refactor

--- apprest/urls/user.py
+++ apprest/urls/user.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from apprest.views import experiment
+from apprest.views.experiment import GetExperimentsByUserName
 
 urlpatterns = [
-    url(r'^(?P<username>\w{1,50})/experiment/$', experiment.get_experiments_from_username),
+  #  url(r'^(?P<username>\w{1,50})/experiment/$', GetExperimentsByUserName.as_view()),
 ]
