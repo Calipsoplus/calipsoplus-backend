432d8ccfa42227c58ea3501a90f270145beb7436
Alex Camps <acampsm@cells.es>
2018-06-06 13:54:04 +0000

    urls modifications by container views

--- apprest/urls/container.py
+++ apprest/urls/container.py
@@ -5,7 +5,7 @@ from apprest.views.container import GetContainersByUserName
 
 urlpatterns = [
     path('run/<username>/<experiment>/', container.run_container, name='run_container'),
-    path('rm/<container_name>/', container.rm_container, name='rm_container'),
-    path('stop/<container_name>/', container.stop_container, name='stop_container'),
+    path('rm/<username>/<container_name>/', container.rm_container, name='rm_container'),
+    path('stop/<username>/<container_name>/', container.stop_container, name='stop_container'),
     path('list/<username>/', GetContainersByUserName.as_view()),
 ]
