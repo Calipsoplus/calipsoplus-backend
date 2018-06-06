03fa7ccc66a74ebe220dd7a4a8d15dccac7c22f3
Alex Camps <acampsm@cells.es>
2018-06-06 10:37:35 +0000

    update urls for containers view

--- apprest/urls/user.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from apprest.views.experiment import GetExperimentsByUserName
-
-urlpatterns = [
-  #  url(r'^(?P<username>\w{1,50})/experiment/$', GetExperimentsByUserName.as_view()),
-]
