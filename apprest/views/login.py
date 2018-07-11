b840a3ee988a1ae8ebe5232e118a00c84dfbbb47
Alex Camps <acampsm@cells.es>
2018-07-11 11:07:23 +0000

    ident

--- apprest/views/login.py
+++ apprest/views/login.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import authenticate, login, logout
-from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import status
 from rest_framework.decorators import api_view
