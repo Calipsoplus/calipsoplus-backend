92fe4bcfe265d126c9fdfdd8ddafa5205206224d
Alex Camps <acampsm@cells.es>
2018-07-23 14:08:52 +0000

    tests changes to favorite

--- apprest/tests/views/test_favorite.py
+++ apprest/tests/views/test_favorite.py
@@ -1,7 +1,9 @@
 import logging
 
 from rest_framework import status
+from rest_framework.utils import json
 
+from apprest.models.user import CalipsoUser
 from apprest.tests.utils import CalipsoTestCase
 
 logger = logging.getLogger(__name__)
@@ -9,13 +11,29 @@ logger = logging.getLogger(__name__)
 
 class FavoriteExperimentViewsTestCase(CalipsoTestCase):
     logger = logging.getLogger(__name__)
-    fixtures = ['users_experiments.json']
+    fixtures = ['users.json', 'experiments.json', 'users_experiments.json']
 
     def setUp(self):
         self.logger.debug('#### setUp START ####')
+        self.calipso_user = CalipsoUser.objects.get(pk=1)
         self.logger.debug('#### setUp END ####')
 
-    def test_swap_favorite(self):
-        response = self.client.put('/favorite/4/', {'favorite': 0})
+    def test_set_favorite(self):
+        url = '/favorite/2/'
+
+        data = {"calipso_user": 1, "calipso_experiment": 2, "favorite": 1}
+        data_str = json.dumps(data)
+
+        self.login_and_check_http_methods(self.calipso_user.user.username, url,
+                                          ['GET', 'POST', 'HEAD', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'])
+
+        response = self.client.put(url, format='json', content_type='application/json', data=data_str)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        base_url = '/experiments/%s/'
+        url = base_url % str(self.calipso_user.user.username)
+        response = self.client.get(url, format='json', content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.json().get('results')[1].get('favorite'), True)
 
--- apprest/tests/views/test_user_experiments.py
+++ apprest/tests/views/test_user_experiments.py
@@ -1,10 +1,9 @@
 import json
+import logging
 
 from rest_framework import status
 
-import logging
-
-from apprest.models.experiment import CalipsoExperiment
+from apprest.models.experiment import CalipsoExperiment, CalipsoUserExperiment
 from apprest.models.user import CalipsoUser
 from apprest.tests.utils import CalipsoTestCase
 
@@ -14,17 +13,23 @@ logger = logging.getLogger(__name__)
 class UserExperimentViewsTestCase(CalipsoTestCase):
     logger = logging.getLogger(__name__)
 
-    fixtures = ['experiments.json', 'users.json']
+    fixtures = ['users.json', 'experiments.json']
 
     def setUp(self):
         self.logger.debug('#### setUp START ####')
 
         self.calipso_user = CalipsoUser.objects.get(pk=1)
+
         self.experiment_1 = CalipsoExperiment.objects.get(pk=1)
         self.experiment_2 = CalipsoExperiment.objects.get(pk=2)
 
-        self.calipso_user.experiments.add(self.experiment_1)
-        self.calipso_user.experiments.add(self.experiment_2)
+        calipso_user_experiment = CalipsoUserExperiment(calipso_user=self.calipso_user,
+                                                        calipso_experiment=self.experiment_1)
+        calipso_user_experiment.save()
+
+        calipso_user_experiment = CalipsoUserExperiment(calipso_user=self.calipso_user,
+                                                        calipso_experiment=self.experiment_2)
+        calipso_user_experiment.save()
 
         self.logger.debug('#### setUp END ####')
 
@@ -39,11 +44,9 @@ class UserExperimentViewsTestCase(CalipsoTestCase):
         # Should return status 200 if everything goes fine
         response = self.client.get(url, format='json', content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.logger.debug('# response.content --> %s' % response.content)
 
         json_content = json.loads(response.content.decode("utf-8"))
-        self.logger.debug('# RESPONSE --> %s' % json_content)
-        self.assertIsInstance(json_content, list)
+
         self.assertGreater(len(json_content), 0)
 
         self.logger.debug('#### TEST get test_get_user_experiments by pk END ####')
