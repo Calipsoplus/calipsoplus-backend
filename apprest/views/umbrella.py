f92531e6b33e8360ee4f10a5c7dd4d3af4909829
acampsm <acampsm@cells.es>
2018-09-18 16:22:03 +0200

    user office mock to get hash..WIP

--- apprest/views/umbrella.py
+++ apprest/views/umbrella.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse
@@ -13,6 +15,8 @@ from django.conf import settings
 
 user_service = CalipsoUserServices()
 
+logger = logging.getLogger(__name__)
+
 
 @api_view(["GET", ])
 def get_umbrella_session_hash(request):
@@ -27,17 +31,19 @@ def get_umbrella_session_hash(request):
 
 @api_view(["GET", ])
 def get_umbrella_frontend(request):
-    return redirect(settings.FRONTEND_CALIPSO + '/autologin/')
+    logger.debug("get, get_umbrella_frontend, redirect to: %s", settings.FRONTEND_CALIPSO + '/autologin')
+    return redirect(settings.FRONTEND_CALIPSO + '/autologin')
 
 
 @api_view(["GET", ])
 def get_umbrella_login(request):
+    logger.debug("get, get_umbrella_login, redirect to: %s", settings.UMBRELLA_LOGIN)
     return redirect(settings.UMBRELLA_LOGIN)
 
 
 @api_view(['POST'])
 def login_umbrella_by_hash(request):
-    logout(request)
+    logger.debug("post, login_umbrella_by_hash")
     try:
         eaa_hash = request.data['EAAHash']
         uid = request.data['uid']
@@ -51,6 +57,7 @@ def login_umbrella_by_hash(request):
     if user is None:
         return JsonResponse({'msg': 'user not found'}, status=404)
     else:
+        logout(request)
         login(request, user, backend='apprest.views.auth_umbrella.ExternalUmbrellaServiceAuthenticationBackend')
         return JSONResponse('Login OK', status=status.HTTP_200_OK)
 
@@ -58,17 +65,18 @@ def login_umbrella_by_hash(request):
 @api_view(["GET", ])
 def get_umbrella_logout_local(request):
     logout(request)
-    return redirect(settings.UMBRELLA_LOGOUT)
+    return JSONResponse("ok logout", status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def find_umbrella_hash_in_user_office_fake(request):
+    logger.debug("post, find_umbrella_hash_in_user_office_fake, redirect")
     try:
         hash = request.data['EAAHash']
-        uid = request.data['uid']
 
         # if hash exists, ok
         return JSONResponse('Login OK', status=status.HTTP_200_OK)
+        #return JSONResponse('Not Found', status=status.HTTP_404_NOT_FOUND)
 
     except Exception as e:
         return JSONResponse("error:%s" % e, status=400)
