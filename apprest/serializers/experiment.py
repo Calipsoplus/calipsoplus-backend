ee4aaff4196e1f79a6553194f4036ef9aef57327
acampsm <acampsm@cells.es>
2018-09-19 14:41:06 +0200

    serializer relation changes

--- apprest/serializers/experiment.py
+++ apprest/serializers/experiment.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from apprest.models.experiment import CalipsoExperiment
+from apprest.models.user import CalipsoUser
 
 
 class CalipsoExperimentSerializer(serializers.ModelSerializer):
@@ -10,7 +12,10 @@ class CalipsoExperimentSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         data = super(CalipsoExperimentSerializer, self).to_representation(instance)
 
-        data['id'] = instance.calipsouserexperiment_set.get().id
-        data['favorite'] = instance.calipsouserexperiment_set.get().favorite
+        user = self.context['request'].user
+        calipso_user = CalipsoUser.objects.get(user=user)
+
+        data['id'] = instance.calipsouserexperiment_set.get(calipso_user=calipso_user).id
+        data['favorite'] = instance.calipsouserexperiment_set.get(calipso_user=calipso_user).favorite
 
         return data
