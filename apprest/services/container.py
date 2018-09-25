92768dd498cd86eb98cdf886e33cb522c2fd98ff
Alex Camps <acampsm@cells.es>
2018-09-25 07:15:41 +0000

    added container image name

--- apprest/services/container.py
+++ apprest/services/container.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import uuid
 
@@ -15,8 +16,6 @@ from apprest.utils.exceptions import QuotaMaxSimultaneousExceeded, QuotaHddExcee
 quota_service = CalipsoUserQuotaServices()
 image_service = CalipsoAvailableImagesServices()
 
-default_public_image_name = "base_image"
-
 
 class CalipsoContainersServices:
 
@@ -26,12 +25,17 @@ class CalipsoContainersServices:
             self.client = docker.DockerClient(tls=False, base_url=settings.DOCKER_URL_DAEMON)
             self.logger.debug('Docker deamon has been initialized')
         except Exception as e:
-            self.logger.critical("Docker deamon not found. %s" % e)
+            self.logger.critical("Docker deamon not found.")
+            self.logger.critical(e)
 
-    def run_container(self, username, experiment):
+
+    def run_container(self, username, experiment, container_public_name):
         """
         Run a new container
         returns a container created
+        :param username
+        :param experiment
+        :param container_public_name:
         """
 
         max_simultaneous = 0
@@ -50,33 +54,35 @@ class CalipsoContainersServices:
             max_cpu += image.cpu
             max_memory += int(image.memory[:-1])
             max_hdd += int(image.hdd[:-1])
+            self.logger.debug("container with public_name=%s" % container.public_name)
 
         try:
             quota_per_user = quota_service.get_default_quota(username=username)[0]
         except Exception as e:
-            self.logger.debug('ERRROR :%s' % e)
+            self.logger.error("Problem to get quota from %s user" % username)
+            self.logger.error(e)
 
         self.logger.debug("num_containers_per_user = %d" % len(list_of_containers))
 
         if max_simultaneous >= quota_per_user.max_simultaneous:
-            self.logger.debug(
+            self.logger.warning(
                 'user:%s used:%d > quota.max:%d' % (username, max_simultaneous, quota_per_user.max_simultaneous))
             raise QuotaMaxSimultaneousExceeded("Max machines exceeded")
 
         if max_cpu >= quota_per_user.cpu:
-            self.logger.debug('user:%s cpu_used:%d > quota.max_cpu:%d' % (username, max_cpu, quota_per_user.cpu))
+            self.logger.warning('user:%s cpu_used:%d > quota.max_cpu:%d' % (username, max_cpu, quota_per_user.cpu))
             raise QuotaCpuExceeded("Max cpus exceeded")
 
         if max_memory >= int(quota_per_user.memory[:-1]):
-            self.logger.debug(
+            self.logger.warning(
                 'user:%s max_ram_used:%dG > quota_user.max_ram:%s' % (username, max_memory, quota_per_user.memory))
             raise QuotaMemoryExceeded("Max memory exceeded")
 
         if max_hdd >= int(quota_per_user.hdd[:-1]):
-            self.logger.debug('user:%s max_hdd:%dG quota.max_hdd:%s' % (username, max_hdd, quota_per_user.hdd))
+            self.logger.warning('user:%s max_hdd:%dG quota.max_hdd:%s' % (username, max_hdd, quota_per_user.hdd))
             raise QuotaHddExceeded("Max hdd exceeded")
 
-        image_selected = image_service.get_available_image(public_name=default_public_image_name)[0]
+        image_selected = image_service.get_available_image(public_name=container_public_name)[0]
 
         try:
             # generate random values for guacamole credentials
@@ -109,6 +115,8 @@ class CalipsoContainersServices:
                                                           )
             """
             storage_opt={'size': '120G'})
+            for log in docker_container.logs(stream=True):
+                print(log)
             """
 
             new_container.container_id = docker_container.id
@@ -116,17 +124,15 @@ class CalipsoContainersServices:
             new_container.container_status = docker_container.status
             new_container.container_info = self.client.api.inspect_container(docker_container.id)
 
-            port = new_container.container_info['NetworkSettings']['Ports']['5901/tcp'][0]['HostPort']
-            ip = new_container.container_info['NetworkSettings']['IPAddress']
-
-            new_container.host_port = ip + " : " + port
+            new_container.host_port = "still not used"
 
             new_container.save()
 
             return new_container
 
         except Exception as e:
-            self.logger.debug(e)
+            self.logger.error("Run container error")
+            self.logger.error(e)
             raise e
 
     def rm_container(self, container_name):
@@ -148,7 +154,8 @@ class CalipsoContainersServices:
             return container
 
         except Exception as e:
-            self.logger.debug(e)
+            self.logger.error("Remove container error")
+            self.logger.error(e)
             raise e
 
     def stop_container(self, container_name):
@@ -170,7 +177,8 @@ class CalipsoContainersServices:
             return container
 
         except Exception as e:
-            self.logger.debug(e)
+            self.logger.error("Stop container error")
+            self.logger.error(e)
             raise e
 
     def list_container(self, username):
@@ -187,5 +195,6 @@ class CalipsoContainersServices:
             return containers
 
         except Exception as e:
-            self.logger.debug(e)
+            self.logger.error("List container error")
+            self.logger.error(e)
             raise e
