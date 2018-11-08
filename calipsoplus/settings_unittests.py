18d33d1337a505e56f7df14194978324a6e3e997
acampsm <acampsm@cells.es>
2018-11-08 12:13:07 +0100

    remove auth db

--- calipsoplus/settings_unittests.py
+++ calipsoplus/settings_unittests.py
@@ -9,10 +9,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'mydatabase'
     },
-    'auth_db': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'auth_db'
-    },
     'guacamole': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'guacamole'
