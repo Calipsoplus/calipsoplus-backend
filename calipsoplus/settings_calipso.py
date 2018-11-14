c46067f3e810bc9580f0313c0c29cb7193093c6b
acampsm <acampsm@cells.es>
2018-11-14 15:23:21 +0100

    new settings keys for data-sets volume config

--- calipsoplus/settings_calipso.py
+++ calipsoplus/settings_calipso.py
@@ -1,5 +1,3 @@
-# may be will be better to be these values in db?
-
 # define generic quota per user
 MAX_CONTAINER_PER_USER = 5
 MAX_RAM_PER_USER = "30G"
@@ -12,3 +10,9 @@ PAGE_SIZE_EXPERIMENTS = 7
 # 0: This would only allow Umbrella authentication. By default, the system does only allow Umbrella authentication.
 # 1: This would allow the user to show the form in order to authenticate locally
 ALLOW_LOCAL_AUTHENTICATION = 1
+
+# Both volumes should be built dynamically, but root of both of them must be declared as settings variables
+# (e.g EXPERIMENTS_DATASETS_ROOT - for the read-only - and EXPERIMENTS_OUTPUT - for the results)--
+EXPERIMENTS_DATASETS_ROOT = "/tmp/data"
+EXPERIMENTS_OUTPUT = "/tmp/results"
+
