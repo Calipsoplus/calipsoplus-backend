d19c2c0ca6d89df8b71b9b8f8c97847bbc682c8f
acampsm <acampsm@cells.es>
2019-02-08 11:53:57 +0100

    new urls for resources and remove container references only use container service to retrieve containers from db

--- apprest/urls/resource.py
+++ apprest/urls/resource.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apprest.views.resource import GetResourcesByUserName, run_resource, rm_resource, stop_resource
 
 urlpatterns = [
-    path('list/<username>/', GetResourcesByUserName.as_view()),
+    path('list/<username>/', GetResourcesByUserName.as_view(), name='list_resource'),
     path('run/<username>/<experiment>/<public_name>/', run_resource, name='run_resource'),
     path('rm/<username>/<resource_name>/<public_name>/', rm_resource, name='rm_resource'),
     path('stop/<username>/<resource_name>/<public_name>/', stop_resource, name='stop_resource'),
