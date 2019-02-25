79945637df3ac64eca7f83a00c543e3ccbc0c1bc
Aidan Campbell <aidan.campbell@esrf.fr>
2019-02-25 16:06:07 +0100

    Added container service for retrieving containers as json

--- apprest/services/container.py
+++ apprest/services/container.py
@@ -7,6 +7,23 @@ class CalipsoContainersServices:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
+    def get_container_by_id(self, cid):
+        """
+        Return a container with a given id
+        :return: single container
+        """
+
+        self.logger.debug('Attempting to get container with id :' + cid)
+        try:
+            container = CalipsoContainer.objects.get(id=cid)
+            self.logger.debug('Returning container ' + cid)
+            return container
+
+        except Exception as e:
+            self.logger.error("Returning container error")
+            self.logger.error(e)
+            raise e
+
     def list_container(self, username):
         """
         List all created containers for a user
@@ -22,5 +39,37 @@ class CalipsoContainersServices:
 
         except Exception as e:
             self.logger.error("List container error")
+            self.logger.error(e)
+            raise e
+
+    def list_all_containers(self):
+        """
+        List all of the created containers
+        :return: list containers
+        """
+        self.logger.debug('Listing all containers')
+
+        try:
+            containers = CalipsoContainer.objects.all()
+            return containers
+
+        except Exception as e:
+            self.logger.error('Unable to list all containers')
+            self.logger.error(e)
+            raise e
+
+    def list_all_active_containers(self):
+        """
+        List all of the containers currently active
+        :return: list containers
+        """
+        self.logger.debug('Listing all active containers')
+
+        try:
+            containers = CalipsoContainer.objects.filter(container_status__in=['created', 'busy'])
+            return containers
+
+        except Exception as e:
+            self.logger.error('Unable to list all containers')
             self.logger.error(e)
             raise e
--- calipsoplus/urls.py
+++ calipsoplus/urls.py
@@ -8,12 +8,17 @@ from apprest.views.image import GetInfoImage, GetAllImages
 from apprest.views.login import login_user, logout_user, get_calipso_settings, get_login_authorization
 from apprest.views.quota import QuotaView, GetUsedQuotaFromUser
 from apprest.views.user import GetUser, GetAllUsers
+from apprest.views.container import ContainerInfo, ActiveContainers, UserContainers
 
 urlpatterns = [
     url(r'^resource/', include('apprest.urls.resource')),
     url(r'^umbrella/', include('apprest.urls.umbrella')),
     path('experiments/<username>/', GetExperimentsByUserName.as_view()),
     url(r'^favorite/(?P<pk>[0-9]+)/$', CalipsoExperimentFavorite.as_view()),
+    path('container/<id>', ContainerInfo.as_view()),
+    path('containers/', ContainerInfo.as_view()),
+    path('containers/active/', ActiveContainers.as_view()),
+    path('containers/<username>/', UserContainers.as_view()),
     path('quota/<username>/', QuotaView.as_view()),
     path('used_quota/<username>/', GetUsedQuotaFromUser.as_view()),
     path('images/', GetAllImages.as_view()),
